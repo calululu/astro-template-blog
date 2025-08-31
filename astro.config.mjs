79bae23b4d56d5f7faea1ac1b16697dcbc5796fd
calululu <ca9delta@gmail.com>
2025-08-31 11:46:30 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sanity from "@sanity/astro";
+
 export default defineConfig({
   site: "https://websiteURLexample.it", //edit website url
   vite: { plugins: [tailwindcss()] },
+  integrations: [
+    // 👇 Update these lines
+    sanity({
+      projectId: "PROJECT-ID",
+      dataset: "DATASET-NAME",
+      useCdn: false, // for static builds
+    }),
+  ],
 });
